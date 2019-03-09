f889aec5f0d46a158c33ce8547f9a32879cfd18c
Chloé Ichinose <chloedv99@gmail.com>
2019-03-09 02:41:04 +0100

    Moving OAuth2 settings to environment variables

---
+++ web/src/Utils/oauth2_settings.js
@@ -0,0 +1,8 @@
+
+export const CLIENT_ID = process.env.CLIENT_ID
+
+export const CLIENT_SECRET = process.env.CLIENT_SECRET
+
+export const REDIRECT_URL = encodeURIComponent(process.env.REDIRECT_URL)
+
+
