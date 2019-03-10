30e2699a03b671d384d9b3f270c088af9d415198
Fiona <fiona.greenwood.sheridan@gmail.com>
2019-03-10 11:21:55 +1100

    Tidied up the code

--- web/src/Components/NotFound.js
+++ web/src/Components/NotFound.js
@@ -5,29 +5,6 @@ import { AVATAR_URL } from './Home'
 import './NotFound.css'
 
 
-const styleHiding = {
-  order: 1,
-  opacity: 0
-}
-
-const styleHidden = {
-  order: 2,
-  opacity: 0
-}
-
-const styleVisible = {
-  order: 1,
-  opacity: 1
-}
-
-
-function delayState(ms) {
-  return time => new Promise(resolve => {
-    setTimeout({}, ms)
-  })
-}
-
-
 class NotFound extends React.PureComponent {
   constructor (props) {
     super(props);
@@ -46,10 +23,10 @@ class NotFound extends React.PureComponent {
         <div className='not-found-message-box'>
           <Avatar url={AVATAR_URL} />
           <span>
-          <span id='oops1'className='not-found-oops' ref={this.ref1}>
+          <span id='oops1'className='not-found-oops'>
             Oops... Page not found~
           </span>
-          <span id='oops2'className='not-found-oops' ref={this.ref2}>
+          <span id='oops2'className='not-found-oops'>
             Redirecting you back home~
           </span>
           </span>
