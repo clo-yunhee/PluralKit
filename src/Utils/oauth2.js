780c9f20b8c8a451c1358cbe11083bbb66cf178c
Chloé Ichinose <chloedv99@gmail.com>
2019-03-11 02:40:42 +0100

    Removed unnecessary oauth2_settings file

--- src/Utils/oauth2.js
+++ src/Utils/oauth2.js
@@ -1,8 +1,11 @@
-import { CLIENT_ID, CLIENT_SECRET, REDIRECT_URL } from './oauth2_settings'
 
+export const CLIENT_ID = process.env.REACT_APP_CLIENT_ID
 
-export { REDIRECT_URL } from './oauth2_settings'
+export const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET
+
+export const REDIRECT_URL = encodeURIComponent(process.env.REACT_APP_REDIRECT_URL)
 
 export const LOGIN_URL = `https://discordapp.com/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL}&response_type=code&scope=identify`
 
 export const ENC_CREDS = btoa(`${CLIENT_ID}:${CLIENT_SECRET}`)
+
